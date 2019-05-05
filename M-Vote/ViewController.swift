24a756b85b38108dcbad4388f6f135d8d79ca6a2
Paul Lascăr <lascar.a.paul@gmail.com>
2019-05-05 16:26:09 +0300

    Initial Commit

---
+++ M-Vote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  M-Vote
+//
+//  Created by Lascar Paul on 05/05/2019.
+//  Copyright © 2019 Lascar Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ M-VoteTests/M_VoteTests.swift
@@ -0,0 +1,34 @@
+//
+//  M_VoteTests.swift
+//  M-VoteTests
+//
+//  Created by Lascar Paul on 05/05/2019.
+//  Copyright © 2019 Lascar Paul. All rights reserved.
+//
+
+import XCTest
+@testable import M_Vote
+
+class M_VoteTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
